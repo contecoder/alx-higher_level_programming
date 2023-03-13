11b865ea67187feae58e321e7902dd69f015555a
contecoder <aboubacarsidikiconteprofs@gmail.com>
2023-03-13 07:09:13 -0700

    retrieve element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def element_at(my_list,idx):
+    """retrive an element
+
+    Args:
+        mylist: list to print
+        idx: the position
+
+    Returns:
+        The return nothing
+    """
+    if(idx < 0):
+        return none
+    elif(idx > len(my_list)):
+        return none
+    else :
+        return my_list[idx]
